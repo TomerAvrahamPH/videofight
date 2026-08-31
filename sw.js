78e0f2dc931fee9a8bc50bce822b9c0ea8e0695d
Tomer Avraham <tomasa25@gmail.com>
2026-08-31 17:07:56 +0300

    Video Fight

---
+++ sw.js
@@ -0,0 +1,6 @@
+const C='vf-1788184781';
+self.addEventListener('install',e=>{ self.skipWaiting();
+  e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-180.png'])));});
+self.addEventListener('activate',e=>{ e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',e=>{ if(e.request.method!=='GET')return;
+  e.respondWith(caches.match(e.request,{ignoreSearch:true}).then(r=>r||fetch(e.request).then(res=>{ const cp=res.clone(); caches.open(C).then(c=>c.put(e.request,cp)); return res; }).catch(()=>caches.match('./index.html'))));});
