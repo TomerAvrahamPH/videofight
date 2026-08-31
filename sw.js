67287aa5505cf9ef876337f673d7ddb010440301
Tomer Avraham <tomasa25@gmail.com>
2026-09-01 00:10:49 +0300

    Full rebuild: martial arts movesets, skeletal bodies, 6 stages with chroma-key intro, combos, supers, finishers, chiptune soundtrack

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='vf-1788200559';
+const C='vf-1788210649';
 self.addEventListener('install',e=>{ self.skipWaiting();
   e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-180.png'])));});
 self.addEventListener('activate',e=>{ e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
