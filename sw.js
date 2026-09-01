246a13443caa08345008c98e821ec6cf08a7b015
Tomer Avraham <tomasa25@gmail.com>
2026-09-01 08:50:54 +0300

    fix: refresh game pages and update cache strategy

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION='video-fight-2026.09.01.2';
+const VERSION='video-fight-2026.09.01.3';
 const CORE=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install', event=>event.waitUntil(caches.open(VERSION).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate', event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key.startsWith('video-fight-')&&key!==VERSION).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
@@ -6,6 +6,12 @@ self.addEventListener('fetch', event=>{
   if(event.request.method!=='GET') return;
   const url=new URL(event.request.url);
   if(url.origin!==location.origin) return;
+  if(event.request.mode==='navigate'){
+    event.respondWith(fetch(event.request).then(response=>{
+      const copy=response.clone(); caches.open(VERSION).then(cache=>cache.put('./index.html',copy)); return response;
+    }).catch(()=>caches.match('./index.html')));
+    return;
+  }
   event.respondWith(caches.match(event.request,{ignoreSearch:true}).then(hit=>hit||fetch(event.request).then(response=>{
     const copy=response.clone(); caches.open(VERSION).then(cache=>cache.put(event.request,copy)); return response;
   }).catch(()=>caches.match('./index.html'))));
