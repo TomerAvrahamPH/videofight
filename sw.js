183fc7b780ce6fcbe62a9bbb9d703743300f9b47
Tomer Avraham <tomasa25@gmail.com>
2026-09-01 08:34:48 +0300

    fix: bypass stale game cache for latest release

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION='video-fight-2026.09.01.1';
+const VERSION='video-fight-2026.09.01.2';
 const CORE=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install', event=>event.waitUntil(caches.open(VERSION).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate', event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key.startsWith('video-fight-')&&key!==VERSION).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
