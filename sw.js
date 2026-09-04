e6bc7075949fa13586ddc5d2a3eb0ffa4f678d24
Tomer Avraham <tomeravraham@MacBook-Pro-sl-Tomer.local>
2026-09-04 18:13:50 +0300

    Render Tomer and Aviv GLB fighters in matches

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION='video-fight-2026.09.04.3d-fighters.2';
+const VERSION='video-fight-2026.09.04.live-3d.1';
 const CORE=['./','./index.html','./peer.min.js','./manifest.webmanifest','./icon.svg',
   './assets/studio-arena-v1.png','./assets/space-arena-v1.png','./assets/barbie-arena-v1.png',
   './assets/nina-charge-v1.png','./assets/fighters/guy-idle-v1.png','./assets/fighters/tomer-idle-v1.png',
@@ -7,7 +7,8 @@ const CORE=['./','./index.html','./peer.min.js','./manifest.webmanifest','./icon
   './assets/fighters/3d/aviv-3d-hurt.png','./assets/fighters/3d/aviv-3d-ko.png',
   './assets/fighters/3d/aviv-3d-air.png','./assets/fighters/3d/tomer-3d-idle.png',
   './assets/fighters/3d/tomer-3d-boom.png','./assets/fighters/3d/tomer-3d-kick.png',
-  './assets/fighters/3d/tomer-3d-guard.png'];
+  './assets/fighters/3d/tomer-3d-guard.png','./vendor/three.min.js','./vendor/GLTFLoader.js',
+  './assets/3d/tomer-v005.glb','./assets/3d/aviv-v004.glb'];
 self.addEventListener('install', event=>event.waitUntil(caches.open(VERSION).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate', event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key.startsWith('video-fight-')&&key!==VERSION).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch', event=>{
