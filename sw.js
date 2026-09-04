7a25bab1b3d6ab6c2ba28a543fa5d3078bed397a
Tomer Avraham <tomeravraham@MacBook-Pro-sl-Tomer.local>
2026-09-04 14:37:27 +0300

    Restore Tomer outfit and correct fighter facing

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION='video-fight-2026.09.04.3d-fighters';
+const VERSION='video-fight-2026.09.04.3d-fighters.2';
 const CORE=['./','./index.html','./peer.min.js','./manifest.webmanifest','./icon.svg',
   './assets/studio-arena-v1.png','./assets/space-arena-v1.png','./assets/barbie-arena-v1.png',
   './assets/nina-charge-v1.png','./assets/fighters/guy-idle-v1.png','./assets/fighters/tomer-idle-v1.png',
